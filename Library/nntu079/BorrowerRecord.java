fb6613fa6335e5a2148bec36340faff9c8908da7
Nguyễn Ngọc Tú <46866558+nntu079@users.noreply.github.com>
2020-10-26 15:50:07 +0700

    Add files via upload

---
+++ Library/nntu079/BorrowerRecord.java
@@ -0,0 +1,19 @@
+package Tu;
+
+public class BorrowerRecord {
+	private String theName;
+
+	public String getTheName() {
+		return theName;
+	}
+
+	public void setTheName(String theName) {
+		this.theName = theName;
+	}
+
+	public BorrowerRecord(String theName) {
+		super();
+		this.theName = theName;
+	}
+	
+}
